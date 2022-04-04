b9531f54d2446cf083b9631de61828ee6700a43e
michwoj01 <michwoj01@gmail.com>
2022-04-04 20:14:58 +0200

    server commit

--- restaurant/src/app/pipes/kitchen.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { KitchenPipe } from './kitchen.pipe';
-
-describe('KitchenPipe', () => {
-  it('create an instance', () => {
-    const pipe = new KitchenPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- restaurant/src/app/pipes/rate.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RatePipe } from './rate.pipe';
-
-describe('RatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new RatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
